21a5354e2efa2ed133849cd760201a387fd5649d
nikosbosse <nikosbosse@gmail.com>
2020-05-25 23:48:10 +0200

    reorganised R files and moved functions

---
+++ R/interval_score.R
@@ -0,0 +1,85 @@
+#' @title Interval Score
+#'
+#' @description
+#' Proper Scoring Rule to score quantile predictions, following Gneiting
+#' and Raftery (2007). Smaller values are better.
+#'
+#' The score is computed as
+#'
+#' \deqn{
+#' score = (upper - lower) + 2/alpha * (lower - true_value) *
+#' 1(true_values < lower) + 2/alpha * (true_value - upper) *
+#' 1(true_value > upper)
+#' }
+#' where $1()$ is the indicator function and alpha is the decimal value that
+#' indicates how much is outside the prediction interval.
+#' To improve usability, the user is asked to provide an interval range in
+#' percentage terms, i.e. interval_range = 90 (percent) for a 90 percent
+#' prediction interval. Correspondingly, the user would have to provide the
+#' 5% and 95% quantiles (the corresponding alpha would then be 0.1).
+#' No specific distribution is assumed,
+#' but the range has to be symmetric (i.e you can't use the 0.1 quantile
+#' as the lower bound and the 0.7 quantile as the upper).
+#'
+#' The interval score is a proper scoring rule that scores a quantile forecast
+#'
+#' @param true_values A vector with the true observed values of size n
+#' @param lower vector of size n with the lower quantile of the given range
+#' @param upper vector of size n with the upper quantile of the given range
+#' @param interval_range the range of the prediction intervals. i.e. if you're
+#' forecasting the 0.05 and 0.95 quantile, the interval_range would be 90.
+#' Can be either a single number or a vector of size n, if the range changes
+#' for different forecasts to be scored. This corresponds to (100-alpha)/100
+#' in Gneiting and Raftery (2007).
+#' @param weigh if TRUE, weigh the score by alpha / 4, so it can be averaged
+#' into an interval score that, in the limit, corresponds to CRPS. Default:
+#' FALSE.
+#' @param verbose if TRUE, gives feedback if provided interval_range seems odd.
+#' @return vector with the scoring values
+#' @examples
+#' true_values <- rnorm(30, mean = 1:30)
+#' interval_range = 90
+#' alpha = (100 - interval_range) / 100
+#' lower = qnorm(alpha/2, rnorm(30, mean = 1:30))
+#' upper = qnorm((1- alpha/2), rnorm(30, mean = 1:30))
+#'
+#' interval_score(true_values = true_values,
+#'                lower = lower,
+#'                upper = upper,
+#'                interval_range = interval_range)
+#' @export
+#' @references Strictly Proper Scoring Rules, Prediction,and Estimation,
+#' Tilmann Gneiting and Adrian E. Raftery, 2007, Journal of the American
+#' Statistical Association, Volume 102, 2007 - Issue 477
+
+
+interval_score <- function(true_values,
+                           lower,
+                           upper,
+                           interval_range = NULL,
+                           weigh = FALSE,
+                           verbose = TRUE) {
+
+  if(is.null(interval_range)) {
+    stop("must provide a range for your prediction interval")
+  }
+
+  if(verbose) {
+    if (interval_range < 1 && interval_range != 0) {
+      message(paste(interval_range),
+              "% interval was provided. Are you sure that's right?", sep = "")
+    }
+  }
+
+  alpha = (100 - interval_range) / 100
+
+  score = (upper - lower) +
+    2/alpha * (lower - true_values) * (true_values < lower) +
+    2/alpha * (true_values - upper) * (true_values > upper)
+
+  if (weigh) score <- score * alpha / 4
+
+  return(score)
+}
+
+
---
+++ R/sharpness.R
@@ -0,0 +1,49 @@
+#' @title Determines sharpness of a probabilistic forecast
+#' @details
+#' Sharpness is the ability of the model to generate predictions within a
+#' narrow range. It is a data-independent measure, and is purely a feature
+#' of the forecasts themselves.
+#'
+#' Shaprness of predictive samples corresponding to one single true value is
+#' measured as the normalised median of the absolute deviation from
+#' the median of the predictive samples. For details, see \link[stats]{mad}
+#'
+#' @param predictions nxN matrix of predictive samples, n (number of rows) being
+#' the number of data points and N (number of columns) the
+#' number of Monte Carlo samples
+#' @importFrom stats mad
+#' @return vector with sharpness values
+#'
+#' @references
+#' Funk S, Camacho A, Kucharski AJ, Lowe R, Eggo RM, Edmunds WJ (2019)
+#' Assessing the performance of real-time epidemic forecasts: A case study of
+#' Ebola in the Western Area region of Sierra Leone, 2014-15.
+#' PLoS Comput Biol 15(2): e1006785.
+#' \url{https://doi.org/10.1371/journal.pcbi.1006785}
+#'
+#' @export
+#' @examples
+#' predictions <- replicate(200, rpois(n = 30, lambda = 1:30))
+#' sharpness(predictions)
+
+sharpness <- function (predictions) {
+
+  # ============== Error handling ==============
+
+  if (missing(predictions)) {
+    stop("predictions argument missing")
+  }
+
+  if (is.data.frame(predictions)) {
+    predictions <- as.matrix(predictions)
+  }
+
+  if (!is.matrix(predictions)) {
+    stop("'predictions' should be a matrix")
+  }
+
+  # ============================================
+
+  sharpness <- apply(predictions, MARGIN = 1, mad)
+  return(sharpness)
+}
