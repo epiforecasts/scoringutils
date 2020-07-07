f0f4089e1c42bf86b65f594bad2b78aa0260d20e
nikosbosse <nikosbosse@gmail.com>
2020-07-07 15:13:27 +0200

    typo correction

--- R/eval_forecasts.R
+++ R/eval_forecasts.R
@@ -241,7 +241,7 @@ eval_forecasts <- function(data,
       res <- res[, .("Interval_Score" = mean(Interval_Score),
                      "calibration" = ifelse(exists("calibration"), mean(calibration), NA),
                      "bias" = ifelse(exists("bias"), mean(sharpness), NA),
-                     "sharpness" = ifelse(exists("sharpness"), mean(sharpness), NA)),),
+                     "sharpness" = ifelse(exists("sharpness"), mean(sharpness), NA)),
                  by = c(by, "range")]
     }
     return(res)
