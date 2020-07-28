3513d4b6dbaf3a08a95296ac6fdc0665ab2d833f
nikosbosse <nikosbosse@gmail.com>
2020-07-28 12:55:57 +0200

    add pivoting functions to switch from long to wide format and vice versa

--- R/utils.R
+++ R/utils.R
@@ -17,26 +17,3 @@ globalVariables(c(".",
                   "upper"))
 
 
-
-quantile_to_wide <- function(data) {
-  data.table::dcast(data, ... ~ boundary + range,
-                    value.var = "predictions")
-}
-
-
-
-
-quantile_to_long <- function(data) {
-  colnames <- colnames(data)
-  ranges <- colnames[grepl("lower", colnames) | grepl("upper", colnames)]
-
-  data <- data.table::melt(data,
-                           id.vars = c("id", "true_values", "model"),
-                           measure.vars = ranges,
-                           variable.name = "range",
-                           value.name = "predictions")
-  data[, boundary := gsub("_.*", "", range)]
-  data[, range := as.numeric(gsub("^.*?_","", range))]
-
-  return(data)
-}
