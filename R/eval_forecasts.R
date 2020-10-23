5c9d3e95bb62f43e7387a2ff87043b638f036712
nikosbosse <nikosbosse@gmail.com>
2020-10-23 18:28:09 +0200

    make interval_code simpler by including the final score in the separate outputs

--- R/eval_forecasts.R
+++ R/eval_forecasts.R
@@ -298,20 +298,16 @@ eval_forecasts <- function(data,
 
     data <- data.table::dcast(data, ... ~ boundary,
                               value.var = "prediction")
-    res <- data[, "interval_score" := do.call(scoringutils::interval_score,
-                                              c(list(true_value,
-                                                     lower,
-                                                     upper,
-                                                     range),
-                                                interval_score_arguments))]
 
-    # this is a bit weird --> find a more elegant solution
+    # this is a bit weird --> find a more elegant solution. Idea is to add the
+    # separate_results = TRUE argument to the function
     if (!("separate_results" %in% names(interval_score_arguments))) {
       interval_score_arguments <- c(interval_score_arguments,
                                     list(separate_results = TRUE))
     }
 
-    res <- data[, c("sharpness",
+    res <- data[, c("interval_score",
+                    "sharpness",
                     "underprediction",
                     "overprediction") := do.call(scoringutils::interval_score,
                                               c(list(true_value,
