7de2fad207669cc127c025ae832f3bcb221f6975
nikosbosse <nikosbosse@gmail.com>
2020-10-27 15:35:32 +0100

    add functionality to show absolute error and quantile coverage

--- R/metrics_point_forecasts.R
+++ R/metrics_point_forecasts.R
@@ -21,3 +21,40 @@ mse <- function(true_values, predictions) {
   mse <- mean((true_values - predictions)^2)
   return(mse)
 }
+
+
+
+
+#' @title Absolute Error of the Median
+#'
+#' @description
+#' Absolute error of the median calculated as
+#'
+#' \deqn{
+#'   abs(true_value - median_prediction)
+#' }
+#'
+#' @param true_values A vector with the true observed values of size n
+#' @param predictions nxN matrix of predictive samples, n (number of rows) being
+#' the number of data points and N (number of columns) the
+#' number of Monte Carlo samples. Alternatively, predictions can just be a vector
+#' of size n
+#' @return vector with the scoring values
+#' @examples
+#' true_values <- rnorm(30, mean = 1:30)
+#' predicted_values <- rnorm(30, mean = 1:30)
+#' ae_median(true_values, predicted_values)
+#' @export
+
+
+ae_median <- function(true_values, predictions) {
+
+  median_predictions <- apply(as.matrix(predictions),
+                              MARGIN = 1,
+                              FUN = mean)
+
+  ae_median <- abs(true_values - median_predictions)
+
+  return(ae_median)
+}
+
