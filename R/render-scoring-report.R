26d28e3a08bbd311bd16b17569c754ba49e73684
nikosbosse <nikosbosse@gmail.com>
2020-11-25 15:45:24 +0100

    add functionality to create automatic reports - needs some tidying and change to data.table

--- R/render-scoring-report.R
+++ R/render-scoring-report.R
@@ -1,9 +1,11 @@
 #' Render a Scoring Report
 #'
 #' @description This function renders a country level TB report.
-#' @param
 #' @param format Character string, defaults to `"html_document"`. The format to render the report to.
 #' See `?rmarkdown::render` for details.
+#' @param truth_data data will all observed values
+#' @param prediction_data dataframe with predictions
+#' @param params parameters. Currently supported are locations, forecast_dates, target_types, horizons
 #' @param interactive Logical, defaults to `FALSE`. When the format allows should graphs be interactive.
 #' @param save_dir Character string, defaults to `NULL`.
 #'  If not given then the report is rendered to a temporary directory (although only if `filename` is also not given).
@@ -23,7 +25,8 @@ render_scoring_report <- function(format = "html_document",
                                   prediction_data,
                                   params = list(locations = "all",
                                                 forecast_dates = "all",
-                                                horizons = "all"),
+                                                horizons = "all",
+                                                target_types = "all"),
                                   interactive = FALSE,
                                   save_dir = NULL,
                                   filename = NULL) {
@@ -62,24 +65,23 @@ render_scoring_report <- function(format = "html_document",
     message("Rendering report to ", save_dir)
   }
 
-
-  if (params$locations[1] == "all") {
+  if ("locations" %in% names(params) & params$locations[1] == "all") {
     locations <- prediction_data$location_name %>%
       unique()
   }
 
-  if (params$forecast_dates[1] == "all") {
+  if ("forecast_dates" %in% names(params) & params$forecast_dates[1] == "all") {
     params$forecast_dates <- prediction_data %>%
       dplyr::pull(forecast_date) %>%
       unique() %>%
       as.character()
   }
-  if (params$target_types[1] == "all") {
+  if ("target_types" %in% names(params) & params$target_types[1] == "all") {
     params$target_types <- prediction_data %>%
       dplyr::pull(target_type) %>%
       unique()
   }
-  if (params$horizons[1] == "all") {
+  if ("horizons" %in% names(params) & params$horizons[1] == "all") {
     horizons <- prediction_data %>%
       dplyr::pull(horizon) %>%
       unique()
