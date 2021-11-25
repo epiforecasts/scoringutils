122c112f9c1628439eac299562e8ccd92757cb35
Nikos Bosse <37978797+nikosbosse@users.noreply.github.com>
2021-11-25 09:34:55 +0100

    Suggestions from code review
    Co-authored-by: Hugo Gruson <Bisaloo@users.noreply.github.com>

--- tests/testthat/test-check_forecasts.R
+++ tests/testthat/test-check_forecasts.R
@@ -1,6 +1,6 @@
 test_that("check_forecasts() function works", {
   check <- check_forecasts(quantile_example_data)
-  expect_equal(class(check), "scoringutils_check")
+  expect_s3_class(check, "scoringutils_check")
 })
 
 
