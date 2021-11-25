2213e37928c4728fb1cb33fe41b383b0bf230641
nikosbosse <nikosbosse@gmail.com>
2021-11-25 10:12:21 +0100

    add more tests for check_forecasts()

--- tests/testthat/test-check_forecasts.R
+++ tests/testthat/test-check_forecasts.R
@@ -7,3 +7,18 @@ test_that("check_forecasts() function works", {
 test_that("check_forecasts() function has an error for empty data.frame", {
   expect_error(check_forecasts(data.frame()))
 })
+
+test_that("check_forecasts() function returns a warning with NA in the data", {
+  check <- check_forecasts(quantile_example_data)
+  expect_equal(unlist(check$warnings),
+               "Some values for `prediction` are NA in the data provided")
+})
+
+test_that("check_forecasts() function returns warnings with NA in the data", {
+  example <- quantile_example_data
+  example[horizon == 7, true_value := NA]
+  check <- check_forecasts(example)
+
+  expect_equal(length(check$warnings), 2)
+})
+
