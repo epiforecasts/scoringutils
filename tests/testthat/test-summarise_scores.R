4de535081b4e6e817b9b5da9a1b43a36c0a0213f
nikosbosse <nikosbosse@gmail.com>
2021-12-22 14:57:04 +0100

    add test for summarise_scores() with no extra arguments

--- tests/testthat/test-summarise_scores.R
+++ tests/testthat/test-summarise_scores.R
@@ -4,5 +4,15 @@ test_that("summarise_scores() works without any arguments", {
 
   scores <- summarise_scores(scores)
   expect_false("quantile" %in% names(scores))
+
+  s2 <- summarise_scores(scores,
+                         by = c("location", "target_end_date", "target_type",
+                                "location_name", "forecast_date", "model",
+                                "horizon"))
+
+  expect_equal(nrow(scores), nrow(s2))
 })
+
+
+
 
