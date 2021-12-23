05f036ba287b9ddee84de6d9ddd8bc2fac1fa99a
nikosbosse <nikosbosse@gmail.com>
2021-12-23 14:39:37 +0100

    Delete package startup message

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Note: The definition of the weighted interval score has slightly changed in version 0.1.5. If you want to use the old definition, use the argument `count_median_twice = TRUE` in the function `score()`")
-}
