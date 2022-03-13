d1728407a5c89cf3066b5ddb5169a161222991cc
nikosbosse <nikosbosse@gmail.com>
2022-03-13 12:56:09 +0100

    create a scoringutils ggplot plot theme

--- R/plot.R
+++ R/plot.R
@@ -1235,3 +1235,24 @@ plot_avail_forecasts <- function(avail_forecasts,
 
   return(plot)
 }
+
+
+
+
+#' @title Scoringutils ggplot2 theme
+#'
+#' @description
+#' A theme for ggplot2 plots used in scoringutils
+#' @return A ggplot2 theme
+#' @importFrom ggplot2 theme theme_minimal element_line
+#' @export
+theme_scoringutils <- function() {
+  theme_minimal() +
+    theme(axis.line = element_line(colour = "grey80"),
+          axis.ticks = element_line(colour = "grey80"),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          panel.border = element_blank(),
+          panel.background = element_blank(),
+          legend.position = "bottom")
+}
