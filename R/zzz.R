92ecf09f66ae9815b06d64e82b7edae7d96e0107
Nikos Bosse <37978797+nikosbosse@users.noreply.github.com>
2022-03-18 16:46:49 +0100

    Update startup message to use the latest tagged version
    Co-authored-by: Hugo Gruson <Bisaloo@users.noreply.github.com>

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage('Note: scoringutils 1.0.0 introduces a lot of breaking changes and we apologise for any inconvenience. If you prefer the old interface, please download version 0.1.7.2 using devtools::install_github("epiforecasts/scoringutils@old")')
+  packageStartupMessage('Note: scoringutils 1.0.0 introduces a lot of breaking changes and we apologise for any inconvenience. If you prefer the old interface, please download version 0.1.8 using remotes::install_github("epiforecasts/scoringutils@v0.1.8")')
 }
