1d608911933a7551a029422463f6d7d5bfe006df
Sam <s.e.abbott12@gmail.com>
2023-01-12 10:38:09 +0000

    the world has seen enough of this message

--- R/zzz.R
+++
@@ -1,3 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage('Note: scoringutils 1.0.0 introduces a lot of breaking changes and we apologise for any inconvenience. If you prefer the old interface, please download version 0.1.8 using remotes::install_github("epiforecasts/scoringutils@v0.1.8")')
-}
