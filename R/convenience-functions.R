b970ca7884953d43cf0cb2e4cd36824dfbb9aeda
nikosbosse <nikosbosse@gmail.com>
2023-03-03 16:32:14 +0100

    function draft that has specific options

--- R/convenience-functions.R
+++ R/convenience-functions.R
@@ -28,21 +28,57 @@
 #' @examples
 #' add_transformation(example_quantile)
 
-add_transformation <- function(data,
-                               fun = function(x) {log(x + 1)},
-                               label = "log",
-                               ...) {
+transform_forecasts <- function(data,
+                                transform = c("log", "sqrt"),
+                                fun = NULL,
+                                add = TRUE,
+                                label = transform,
+                                ...) {
 
-  data <- as.data.table(data)
-  transformed_data <- data.table::copy(data)
+  if (is.null(fun)) {
+    if (transform == "log") {
+      if (any(data$true_value < 0 | any(data$prediction < 0))) {
+        stop("Can't apply log transformation, values < 0 present")
+      } else if (any(data$true_value == 0 | any(data$prediction == 0))) {
+        fun <- function(x) {log(x + 1)}
+        message("Detected zeros in the data, using log(x + 1) as transformation instead.")
+      } else {
+        fun <- log
+      }
+    }
 
-  data[, scale := "natural"]
-  transformed_data[, scale := label]
+    if (transform == "sqrt") {
+      if (any(data$true_value < 0 | any(data$prediction < 0))) {
+        stop("Can't apply sqrt transformation, values < 0 present")
+      } else {
+        fun <- sqrt
+      }
+    }
+  }
+
+  # check if a column called "scale" is already present and if so, only
+  # restrict to transformations of the original data
+  if ("scale" %in% colnames(data)) {
+    if (!("natural" %in% data$scale)) {
+      stop("If a column 'scale' is present, entries with scale =='natural' are needed")
+    }
+    transformed_data <- data.table::copy(data[data$scale == "natural", ])
+  } else {
+    transformed_data <- data.table::copy(data)
+  }
 
   transformed_data[, prediction := fun(prediction, ...)]
   transformed_data[, true_value := fun(true_value, ...)]
 
-  out <- rbind(data, transformed_data)
+  if (add) {
+    data <- as.data.table(data)
+    data[, scale := "natural"]
+    transformed_data[, scale := label]
+    out <- rbind(data, transformed_data)
+    return(out)
+  }
+
+
 
   return(out)
 }
