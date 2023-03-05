e977438d379a8e7252dc9e4fc80014aee269f693
nikosbosse <nikosbosse@gmail.com>
2023-03-05 14:54:33 +0100

    update suggested draft for transform_forecasts, add documentation and tests

--- R/convenience-functions.R
+++ R/convenience-functions.R
@@ -1,14 +1,37 @@
 #' @title Add transformations
 #'
 #' @description Add transformations of the forecasts and observations for
-#' later scoring. For more information on why this might be desirable, check
-#' out the linked reference.
+#' later scoring.
+#'
+#' @details There are a few reasons, depending on the circumstances, for
+#' why this might be desirable (check out the linked reference for more info).
+#' In epidemiology, for example, it may be useful to log-transform incidence
+#' counts before evaluating forecasts using scores such as the weighted interval
+#' score (WIS) or the continuous ranked probability score (CRPS).
+#' Log-transforming forecasts and observations changes the interpretation of
+#' the score from a measure of absolute distance between forecast and
+#' observation to a score that evaluates a forecast of the exponential growth
+#' rate. Another motivation can be to apply a variance-stabilising
+#' transformation or to standardise incidence counts by population.
+#'
+#' Note that if you want to apply a transformation, it is important to transform
+#' the forecasts and observations and then apply the score. Applying a
+#' transformation after the score risks losing propriety of the proper scoring
+#' rule.
 #'
 #' @inheritParams score
 #' @param fun A function used to transform both true values and predictions
+#' @param add whether or not to add a transformed version of the data to the
+#' currently existing data (default is TRUE). If selected, the data gets
+#' transformed and appended to the existing data frame, making it possible to
+#' use the outcome directly in `score()`. An additional column, 'scale', gets
+#' created that denotes which rows or untransformed ('scale' has the value
+#' "natural") and which have been transformed ('scale' has the value passed to
+#' the argument `label`).
 #' @param label A string for the newly created 'scale' column to denote the
-#' newly transformed values.
-#' @return A data.table that includes the original data as well as a
+#' newly transformed values. Only relevant if `add = TRUE`.
+#' @return A data.table with either a transformed version of the data, or one
+#' with both the untransformed and the transformed data. includes the original data as well as a
 #' transformation of the original data. There will be one additional column,
 #' 'scale', present which will be set to "natural" for the untransformed
 #' forecasts.
@@ -26,35 +49,53 @@
 
 #' @keywords check-forecasts
 #' @examples
-#' add_transformation(example_quantile)
+#'
+#' # replace negative values with zero
+#' transformed <- example_quantile |>
+#'   transform_forecasts(fun = function(x) {pmax(0, x)}, add = FALSE)
+#'
+#' # add log transformed forecasts (produces a warning as some values are zero)
+#' transform_forecasts(transformed)
+#'
+#' # specifying an offset manually for the log transformation removes the warning
+#' transform_forecasts(transformed, offset = 1)
+
 
 transform_forecasts <- function(data,
-                                transform = c("log", "sqrt"),
-                                fun = NULL,
+                                fun = log,
                                 add = TRUE,
-                                label = transform,
+                                label = "log",
                                 ...) {
 
-  if (is.null(fun)) {
-    if (transform == "log") {
-      if (any(data$true_value < 0 | any(data$prediction < 0))) {
-        stop("Can't apply log transformation, values < 0 present")
-      } else if (any(data$true_value == 0 | any(data$prediction == 0))) {
-        fun <- function(x) {log(x + 1)}
-        message("Detected zeros in the data, using log(x + 1) as transformation instead.")
-      } else {
-        fun <- log
-      }
+  if (identical(fun, log)) {
+    if (any(data$true_value < 0, na.rm = TRUE) |
+        any(data$prediction < 0, na.rm = TRUE)) {
+      stop("Can't apply log transformation, values < 0 present")
     }
 
-    if (transform == "sqrt") {
-      if (any(data$true_value < 0 | any(data$prediction < 0))) {
-        stop("Can't apply sqrt transformation, values < 0 present")
-      } else {
-        fun <- sqrt
-      }
+    zeros_present <- (any(data$true_value == 0, na.rm = TRUE) |
+                        any(data$prediction == 0, na.rm = TRUE))
+    if (!("offset" %in% names(list(...))) && zeros_present) {
+      offset_default <- 1
+      warning(
+        paste0(
+          "Detected zeros in the data, using log(x + 1) as transformation instead. ",
+          "You can specify offset = 1 (or any different offset) to remove this warning."
+        )
+      )
+    } else if (!("offset" %in% names(list(...)))) {
+      offset_default <- 0
+    }
+    fun <- function(x, offset = offset_default) {log(x + offset)}
+  }
+
+  if (identical(fun, sqrt)) {
+    if (any(data$true_value < 0, na.rm = TRUE) |
+        any(data$prediction < 0, na.rm = TRUE)) {
+      stop("Can't apply sqrt transformation, values < 0 present")
     }
   }
+
 
   # check if a column called "scale" is already present and if so, only
   # restrict to transformations of the original data
@@ -75,10 +116,8 @@ transform_forecasts <- function(data,
     data[, scale := "natural"]
     transformed_data[, scale := label]
     out <- rbind(data, transformed_data)
-    return(out)
+    return(out[])
   }
-
-
-
-  return(out)
+  out <- transformed_data
+  return(out[])
 }
--- tests/testthat/test-convenience-functions.R
+++ tests/testthat/test-convenience-functions.R
@@ -1,12 +1,19 @@
-test_that("function add_transformation works", {
+test_that("function transform_forecasts works", {
 
-  predictions <- example_quantile$prediction
-
-  one <- suppressWarnings(add_transformation(example_quantile))
-  expect_equal(one$prediction, c(predictions, log(predictions + 1)))
-
-  two <- suppressWarnings(
-    add_transformation(example_quantile, fun = sqrt, label = "sqrt")
+  predictions_original <- example_quantile$prediction
+  predictions <- transform_forecasts(
+    example_quantile,
+    fun = function(x) pmax(0, x),
+    add = FALSE
   )
-  expect_equal(two$prediction, c(predictions, sqrt(predictions)))
+
+  expect_equal(predictions$prediction, pmax(0, predictions_original))
+
+  one <- transform_forecasts(predictions, offset = 1)
+  expect_equal(one$prediction,
+               c(predictions$prediction, log(predictions$prediction + 1)))
+
+  two <- transform_forecasts(predictions, fun = sqrt, label = "sqrt")
+  expect_equal(two$prediction,
+               c(predictions$prediction, sqrt(predictions$prediction)))
 })
