1207d2aecf615301d8bfe872f8ab3a4fc5d8a305
nikosbosse <nikosbosse@gmail.com>
2023-10-25 13:37:05 +0200

    Add tests for get_duplicate_forecasts, update documentation

--- tests/testthat/test-find_duplicates.R
+++
@@ -1,8 +0,0 @@
-test_that("get_duplicate_forecasts() works as expected", {
-  expect_equal(nrow(get_duplicate_forecasts(example_quantile)), 0)
-  expect_equal(
-    nrow(
-      get_duplicate_forecasts(rbind(example_quantile, example_quantile[1000:1010]))),
-    22
-  )
-})
