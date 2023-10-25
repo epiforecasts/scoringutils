7b1933407a120dab480f5db8e42b8e4372571de9
nikosbosse <nikosbosse@gmail.com>
2023-10-25 15:56:56 +0200

    fix point forecast example data

--- inst/create-example-data.R
+++ inst/create-example-data.R
@@ -121,7 +121,7 @@ usethis::use_data(example_quantile, overwrite = TRUE)
 
 # create data with point forecasts ---------------------------------------------
 example_point <- data.table::copy(example_quantile)
-exmple_point <- example_point[quantile %in% c(NA, 0.5)][, quantile := NULL]
+example_point <- example_point[quantile %in% c(NA, 0.5)][, quantile := NULL]
 usethis::use_data(example_point, overwrite = TRUE)
 
 
