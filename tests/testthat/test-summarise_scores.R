796155be9f604c867f85d0dd614bd8e3709440f2
nikosbosse <nikosbosse@gmail.com>
2023-10-28 13:20:30 +0200

    fix failing tests for add_pairwise_comparison (leaving aside an issue about how forecast units are handled)

--- tests/testthat/test-summarise_scores.R
+++ tests/testthat/test-summarise_scores.R
@@ -36,58 +36,41 @@ test_that("summarise_scores() works with point forecasts in a quantile format", 
 
   scores <- suppressMessages(score(ex))
 
-  summarise_scores(scores, by = "model", na.rm = TRUE)
+  scores_summarised <-summarise_scores(scores, by = "model", na.rm = TRUE)
   expect_warning(
     expect_warning(
-      summarise_scores(
-        scores, by = "model", relative_skill = TRUE, na.rm = TRUE)
+      add_pairwise_comparison(scores_summarised)
     )
   )
 
   scores_point <- suppressMessages(score(example_point))
+  summarised_scores <- summarise_scores(scores_point, by = "model")
 
-  # expect_warning(
-  #   expect_warning(
-  #     summarise_scores(
-  #       scores_point, by = "model", relative_skill = TRUE, na.rm = TRUE)
-  #   )
-  # )
+  # this currently does not work and needs to be fixed
+  # add_pairwise_comparison(summarised_scores, relative_skill_metric = "se_point")
 })
 
 test_that("summarise_scores() can compute relative measures", {
   ex <- data.table::copy(example_quantile)
   scores <- suppressMessages(score(ex))
 
-  expect_equal(
-    summarise_scores(
-      scores, by = "model", relative_skill = TRUE
-    )[, relative_skill],
-    c(1.6, 0.81, 0.75, 1.03), tolerance = 0.01
+  scores_with <- add_pairwise_comparison(
+    summarise_scores(scores, by = "model")
   )
 
   expect_equal(
-    summarise_scores(
-      scores, by = "model", relative_skill = TRUE,
-      relative_skill_metric = "ae_median"
-    )[, relative_skill],
+    scores_with[, relative_skill],
+    c(1.6, 0.81, 0.75, 1.03), tolerance = 0.01
+  )
+
+  scores_with <- add_pairwise_comparison(
+    summarise_scores(scores, by = "model"),
+    relative_skill_metric = "ae_median"
+  )
+
+  expect_equal(
+    scores_with[, relative_skill],
     c(1.6, 0.78, 0.77, 1.04), tolerance = 0.01
-  )
-})
-
-test_that("summarise_scores() metric is deprecated", {
-  ex <- data.table::copy(example_quantile)
-  scores <- suppressMessages(score(ex))
-
-  expect_equal(
-    suppressWarnings(summarise_scores(
-    scores, by = "model", metric = "auto", relative_skill = TRUE
-  ))[, relative_skill],
-    c(1.6, 0.81, 0.75, 1.03), tolerance = 0.01
-  )
-  expect_snapshot(
-    x <- summarise_scores(
-      scores, by = "model", metric = "auto", relative_skill = TRUE
-    )
   )
 })
 
