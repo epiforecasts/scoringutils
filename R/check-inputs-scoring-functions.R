89f716142652a59d713f6e5000cd4e52d17db529
nikosbosse <nikosbosse@gmail.com>
2023-11-03 18:13:33 +0100

    Add functions to check inputs for scoring functions that accept an interval format

--- R/check-inputs-scoring-functions.R
+++ R/check-inputs-scoring-functions.R
@@ -85,6 +85,62 @@ check_input_quantile <- function(observed, predicted, quantile) {
 }
 
 
+#' @title Assert that inputs are correct for interval-based forecast
+#' @description Function assesses whether the inputs correspond to the
+#' requirements for scoring interval-based forecasts.
+#' @param observed Input to be checked. Should be a numeric vector with the
+#' observed values of size n
+#' @param lower Input to be checked. Should be a numeric vector of size n that
+#' holds the predicted value for the lower bounds of the prediction intervals.
+#' @param upper Input to be checked. Should be a numeric vector of size n that
+#' holds the predicted value for the upper bounds of the prediction intervals.
+#' @param range Input to be checked. Should be a vector of size n that
+#' denotes the interval range in percent. E.g. a value of 50 denotes a
+#' (25%, 75%) prediction interval.
+#' @importFrom rlang warn
+#' @inherit document_assert_functions return
+#' @keywords internal
+assert_input_interval <- function(observed, lower, upper, range) {
+
+  assert_numeric_vector(observed, min.len = 1)
+  n <- length(observed)
+  assert_numeric_vector(lower, len = n)
+  assert_numeric_vector(upper, len = n)
+  assert_numeric_vector(range, len = n, lower = 0, upper = 100)
+
+  diff <- upper - lower
+  if (any(diff < 0)) {
+    stop(
+      "All values in `upper` need to be greater than or equal to ",
+      "the corresponding values in `lower`"
+    )
+  }
+  if (any(range > 0 & range < 1, na.rm = TRUE)) {
+    msg <- paste(
+      "Found interval ranges between 0 and 1. Are you sure that's right? An",
+      "interval range of 0.5 e.g. implies a (49.75%, 50.25%) prediction",
+      "interval. If you want to score a (25%, 75%) prediction interval, set",
+      "`interval_range = 50`."
+    )
+    rlang::warn(
+      message = msg, .frequency = "once",
+      .frequency_id = "small_interval_range"
+    )
+  }
+  return(invisible(NULL))
+}
+
+
+#' @title Check that inputs are correct for interval-based forecast
+#' @inherit assert_input_interval params description
+#' @inherit check_input_sample return description
+#' @keywords check-inputs
+check_input_interval <- function(observed, lower, upper, range) {
+  result <- check_try(assert_input_quantile(observed, lower, upper, range))
+  return(result)
+}
+
+
 #' @title Assert that inputs are correct for binary forecast
 #' @description Function assesses whether the inputs correspond to the
 #' requirements for scoring binary forecasts.
