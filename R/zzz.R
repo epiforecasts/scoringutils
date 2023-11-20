586b2d0690bb6248543cfed584ebc86d3644ba3f
nikosbosse <nikosbosse@gmail.com>
2023-11-20 20:59:37 +0100

    Fix linting issue

--- R/zzz.R
+++ R/zzz.R
@@ -3,5 +3,6 @@
     "Note: scoringutils is currently undergoing major development changes ",
     "(with an update planned for February 1st, 2024). We would very much ",
     "appreciate your opinions and feedback on what should be included in this ",
-    "major update: https://github.com/epiforecasts/scoringutils/discussions/333")
+    "major update: https://github.com/epiforecasts/scoringutils/discussions/333"
+  )
 }
