5b7aa12f49ca26d6828eda58d16e51e8d9cd3d40
nikosbosse <nikosbosse@gmail.com>
2023-11-28 14:00:40 +0100

    remove `plot_avail_forecasts()`

--- R/plot.R
+++ R/plot.R
@@ -1011,44 +1011,6 @@ plot.scoringutils_available_forecasts <- function(x,
 }
 
 
-#' @title Visualise Where Forecasts Are Available `r lifecycle::badge("deprecated")`
-#'
-#' @description
-#' Old version of [plot.scoringutils_available_forecasts()] for compatibility.
-#' @inheritParams plot.scoringutils_available_forecasts
-#' @param available_forecasts an S3 object of class "scoringutils_available_forecasts"
-#' as produced by [available_forecasts()]
-#' @param y character vector of length one that denotes the name of the column
-#' to appear on the y-axis of the plot. Default is "model".
-#' @param x character vector of length one that denotes the name of the column
-#' to appear on the x-axis of the plot. Default is "forecast_date".
-#' @param make_x_factor logical (default is TRUE). Whether or not to convert
-#' the variable on the x-axis to a factor. This has an effect e.g. if dates
-#' are shown on the x-axis.
-#' @export
-plot_avail_forecasts <- function(available_forecasts,
-                                 y = "model",
-                                 x = "forecast_date",
-                                 make_x_factor = TRUE,
-                                 show_numbers = TRUE) {
-
-  lifecycle::deprecate_warn(
-    "1.2.2", "plot_avail_forecasts()",
-    "plot()"
-  )
-
-  plot.scoringutils_available_forecasts(
-    x = available_forecasts,
-    yvar = y,
-    xvar = x,
-    make_xvar_factor = make_x_factor,
-    show_numbers = show_numbers
-  )
-}
-
-
-
-
 #' @title Plot Correlation Between Metrics
 #'
 #' @description
