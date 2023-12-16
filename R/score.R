fedd9c939ac26c8c319aee5eb47e04c2bf00f152
nikosbosse <nikosbosse@gmail.com>
2023-12-16 13:13:28 +0100

    Update documentation for `score()`

--- R/score.R
+++ R/score.R
@@ -1,94 +1,25 @@
 #' @title Evaluate forecasts in a data.frame format
-#'
 #' @description `score()` applies a selection of scoring metrics to a data.frame
 #' of forecasts. It is the workhorse of the `scoringutils` package.
 #' `score()` is a generic that dispatches to different methods depending on the
-#' class of the input data. The default method is `score.default()`, which
-#' validates the input, assigns as class based on the forecast type, and then
-#' calls `score()` again to dispatch to the appropriate method. See below for
-#' more information on how forecast types are determined.
+#' class of the input data.
 #'
-#' @details
-#' **Forecast types and input format**
-#'
-#' Various different forecast types / forecast formats are supported. At the
-#' moment, those are
-#' - point forecasts
-#' - binary forecasts ("soft binary classification")
-#' - Probabilistic forecasts in a quantile-based format (a forecast is
-#' represented as a set of predictive quantiles)
-#' - Probabilistic forecasts in a sample-based format (a forecast is represented
-#' as a set of predictive samples)
-#'
-#' Forecast types are determined based on the columns present in the input data.
-#'
-#' *Point forecasts* require a column `observed` of type numeric and a column
-#' `predicted` of type numeric.
-#'
-#' *Binary forecasts* require a column `observed` of type factor with exactly
-#' two levels and a column `predicted` of type numeric with probabilities,
-#' corresponding to the probability that `observed` is equal to the second
-#' factor level. See details [here][brier_score()] for more information.
-#'
-#' *Quantile-based forecasts* require a column `observed` of type numeric,
-#' a column `predicted` of type numeric, and a column `quantile` of type numeric
-#' with quantile-levels (between 0 and 1).
-#'
-#' *Sample-based forecasts* require a column `observed` of type numeric,
-#' a column `predicted` of type numeric, and a column `sample_id` of type
-#' numeric with sample indices.
-#'
-#' For more information see the vignettes and the example data
-#' ([example_quantile], [example_continuous], [example_integer],
-#' [example_point()], and [example_binary]).
-#'
-#' **Forecast unit**
-#'
-#' In order to score forecasts, `scoringutils` needs to know which of the rows
-#' of the data belong together and jointly form a single forecasts. This is
-#' easy e.g. for point forecast, where there is one row per forecast. For
-#' quantile or sample-based forecasts, however, there are multiple rows that
-#' belong to single forecast.
-#'
-#' The *forecast unit* or *unit of a single forecast* is then described by the
-#' combination of columns that uniquely identify a single forecast.
-#' For example, we could have forecasts made by different models in various
-#' locations at different time points, each for several weeks into the future.
-#' The forecast unit could then be described as
-#' `forecast_unit = c("model", "location", "forecast_date", "forecast_horizon")`.
-#' `scoringutils` automatically tries to determine the unit of a single
-#' forecast. It uses all existing columns for this, which means that no columns
-#' must be present that are unrelated to the forecast unit. As a very simplistic
-#' example, if you had an additional row, "even", that is one if the row number
-#' is even and zero otherwise, then this would mess up scoring as `scoringutils`
-#' then thinks that this column was relevant in defining the forecast unit.
-#'
-#' In order to avoid issues, we recommend using the function
-#' [set_forecast_unit()] to determine the forecast unit manually.
-#' The function simply drops unneeded columns, while making sure that all
-#' necessary, 'protected columns' like "predicted" or "observed" are retained.
-#'
-#' **Validating inputs**
-#'
-#' We recommend that users validate their input prior to scoring using the
-#' function [validate()] (though this will also be run internally by [score()]).
-#' The function checks the input data and provides helpful information.
-#'
-#'
-#' **Further help**
-#'
+#' We recommend that users call [as_forecast()] prior to calling `score()` to
+#' validate the input data and convert it to a forecast object (though
+#' `score.default()` will do this if it hasn't happened before).
+#' See below for more information on forecast types and input formats.
 #' For additional help and examples, check out the [Getting Started
 #' Vignette](https://epiforecasts.io/scoringutils/articles/scoringutils.html) as
 #' well as the paper [Evaluating Forecasts with scoringutils in
 #' R](https://arxiv.org/abs/2205.07090).
-#'
+#' @inheritSection forecast_types Forecast types and input format
+#' @inheritSection forecast_types Forecast unit
 #' @param data A data.frame or data.table with predicted and observed values.
 #' @param metrics A named list of scoring functions. Names will be used as
 #' column names in the output. See [metrics_point()], [metrics_binary()],
 #' `metrics_quantile()`, and [metrics_sample()] for more information on the
 #' default metrics used.
 #' @param ... additional arguments
-#'
 #' @return A data.table with unsummarised scores. This will generally be
 #' one score per forecast (as defined by the unit of a single forecast).
 #'
@@ -97,9 +28,7 @@
 #' for individual quantiles. You can call [summarise_scores()]) on the
 #' unsummarised scores to obtain one score per forecast unit for quantile-based
 #' forecasts.
-#'
 #' @importFrom data.table ':=' as.data.table
-#'
 #' @examples
 #' library(magrittr) # pipe operator
 #' data.table::setDTthreads(1) # only needed to avoid issues on CRAN
@@ -125,13 +54,11 @@
 #' score(example_integer)
 #' score(example_continuous)
 #' }
-#'
 #' @author Nikos Bosse \email{nikosbosse@@gmail.com}
 #' @references
 #' Bosse NI, Gruson H, Cori A, van Leeuwen E, Funk S, Abbott S
 #' (2022) Evaluating Forecasts with scoringutils in R.
 #' \doi{10.48550/arXiv.2205.07090}
-#'
 #' @export
 
 score <- function(data, ...) {
