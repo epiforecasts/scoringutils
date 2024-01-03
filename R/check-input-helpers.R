afb8a39c7c30954c72a91fb38770edff0a32a02d
nikosbosse <nikosbosse@gmail.com>
2024-01-03 16:43:10 +0100

    Omit `NA` values when checking the number of samples / quantiles per forceast

--- R/check-input-helpers.R
+++ R/check-input-helpers.R
@@ -211,6 +211,7 @@ assure_model_column <- function(data) {
 #' @inherit document_check_functions params return
 #' @keywords internal_input_check
 check_number_per_forecast <- function(data, forecast_unit) {
+  data <- na.omit(data)
   # check whether there are the same number of quantiles, samples --------------
   data[, scoringutils_InternalNumCheck := length(predicted), by = forecast_unit]
   n <- unique(data$scoringutils_InternalNumCheck)
