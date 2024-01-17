a864bc5bca4bc210c80a791236603994dd2a6f4e
nikosbosse <nikosbosse@gmail.com>
2024-01-16 19:49:07 -0500

    Add unit tests for functions that create an object of class `scores`

--- tests/testthat/test-score.R
+++ tests/testthat/test-score.R
@@ -1,3 +1,48 @@
+# =============================================================================
+# Check creation of objects of class `scores`
+# =============================================================================
+
+test_that("new_scores() works", {
+  expect_equal(
+    class(new_scores(data.frame(), score_names = "")),
+    c("scores", "data.table", "data.frame")
+  )
+
+  expect_error(
+    new_scores(data.frame()),
+    "missing, with no default"
+  )
+})
+
+test_that("as_scores() works", {
+  expect_equal(
+    class(scoringutils:::as_scores(data.frame(wis = 1), score_names = "wis")),
+    c("scores", "data.table", "data.frame")
+  )
+  expect_warning(
+    scoringutils:::as_scores(data.frame(), score_names = "wis"),
+    "The following scores have been previously computed"
+  )
+})
+
+test_that("validate_scores() works", {
+  expect_error(
+    validate_scores(data.frame()),
+    "Must inherit from class 'scores'"
+  )
+})
+
+test_that("Output of `score()` has the class `scores()`", {
+  expect_no_condition(validate_scores(scores_point))
+  expect_no_condition(validate_scores(scores_binary))
+  expect_no_condition(validate_scores(scores_continuous))
+  expect_no_condition(validate_scores(scores_quantile))
+})
+
+# =============================================================================
+# `score()`
+# =============================================================================
+
 # common error handling --------------------------------------------------------
 test_that("function throws an error if data is missing", {
   expect_error(suppressMessages(score(data = NULL)))
