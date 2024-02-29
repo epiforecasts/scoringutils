7f844ee27a0e06d5527433367d24c4695a3abdb0
nikosbosse <nikosbosse@gmail.com>
2024-02-28 22:02:06 -0600

    Add tests to check behaviour of `get_coverage()` when prediction intervals are not symmetric

--- tests/testthat/test-add_coverage.R
+++ tests/testthat/test-add_coverage.R
@@ -19,3 +19,27 @@ test_that("get_coverage() outputs an object of class c('data.table', 'data.frame
   cov <- get_coverage(ex)
   expect_s3_class(cov, c("data.table", "data.frame"), exact = TRUE)
 })
+
+test_that("get_coverage() can deal with non-symmetric prediction intervals", {
+  # the expected result is that `get_coverage()` just works. However,
+  # all interval coverages with missing values should just be `NA`
+  test <- data.table::copy(example_quantile)
+  test <- test[!quantile_level %in% c(0.2, 0.3, 0.5)]
+
+  expect_no_condition(cov <- get_coverage(test))
+
+  prediction_intervals <- get_range_from_quantile(c(0.2, 0.3, 0.5))
+
+  missing <- cov[interval_range %in% prediction_intervals]
+  not_missing <- cov[!interval_range %in% prediction_intervals]
+
+  expect_true(all(is.na(missing$interval_coverage)))
+  expect_false(any(is.na(not_missing)))
+
+  # test for a version where values are not missing, but just `NA`
+  # since `get_coverage()` calls `na.omit`, the result should be the same.
+  test <- data.table::copy(example_quantile)
+  test <- test[quantile_level %in% c(0.2, 0.3, 0.5), predicted := NA]
+  cov2 <- get_coverage(test)
+  expect_equal(cov, cov2)
+})
