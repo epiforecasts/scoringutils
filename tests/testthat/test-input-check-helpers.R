2829b502576d3455250596e13c26d2f4f7be5bf6
James Azam <james.m.azam@gmail.com>
2024-03-01 13:58:51 +0000

    Add tests for input checkers

--- tests/testthat/test-input-check-helpers.R
+++ tests/testthat/test-input-check-helpers.R
@@ -40,4 +40,126 @@ test_that("assure_model_column works", {
     assure_model_column(test[, model := NULL]),
     "There is no column called `model` in the data."
   )
+  expect_true(
+    setequal(
+      assure_model_column(example_binary),
+      example_binary
+    )
+  )
+})
+
+test_that("check_number_per_forecast works", {
+  expect_identical(
+    capture.output(
+      check_number_per_forecast(
+        example_binary, forecast_unit = "location_name"
+      )
+    ),
+    paste(
+      "[1] \"Some forecasts have different numbers of rows",
+      "(e.g. quantiles or samples). scoringutils found: 224, 215.",
+      "This may be a problem (it can potentially distort scores,",
+      "making it more difficult to compare them),",
+      "so make sure this is intended.\""
+    )
+  )
+  expect_true(
+    check_number_per_forecast(
+      example_binary
+    )
+  )
+})
+
+test_that("check_no_NA_present works", {
+  expect_identical(
+    capture.output(
+      check_no_NA_present(example_binary, columns = "predicted")
+    ),
+    paste(
+      "[1] \"Checking `data`: 144 values in column `predicted`` are NA and",
+      "corresponding rows will be removed. This is fine if not unexpected.\""
+    )
+  )
+  expect_true(
+    check_no_NA_present(example_binary, columns = "location")
+  )
+})
+
+test_that("check_duplicates works", {
+  example_bin <- rbind(example_binary[1:2, ], example_binary[1:2, ])
+  expect_identical(
+    capture.output(
+      check_duplicates(example_bin)
+    ),
+    paste(
+      "[1] \"There are instances with more than one forecast for the same",
+      "target. This can't be right and needs to be resolved.",
+      "Maybe you need to check the unit of a single forecast and add",
+      "missing columns? Use the function get_duplicate_forecasts() to",
+      "identify duplicate rows\""
+    )
+  )
+  expect_true(
+    check_duplicates(example_binary)
+  )
+})
+
+test_that("check_columns_present works", {
+  expect_identical(
+    capture.output(
+      check_columns_present(example_binary, c("loc1", "loc2", "loc3"))
+    ),
+    paste(
+      "[1] \"Columns 'loc1', 'loc2', 'loc3' not found in data\""
+    )
+  )
+  expect_identical(
+    capture.output(
+      check_columns_present(example_binary, c("loc1"))
+    ),
+    paste(
+      "[1] \"Column 'loc1' not found in data\""
+    )
+  )
+  expect_true(
+    check_columns_present(example_binary, c("location_name"))
+  )
+  expect_true(
+    check_columns_present(example_binary, columns = NULL)
+  )
+})
+
+test_that("check_has_attribute works", {
+  expect_identical(
+    capture.output(
+      check_has_attribute(example_binary, "forecast")
+    ),
+    "[1] \"Found no attribute `forecast`\""
+  )
+  expect_true(
+    check_has_attribute(example_binary, "names")
+  )
+})
+
+test_that("test_columns_not_present works", {
+  expect_true(
+    test_columns_not_present(example_binary, "sample_id")
+  )
+  expect_false(
+    test_columns_not_present(example_binary, "location")
+  )
+})
+
+test_that("check_data_columns works", {
+  # Add quantile_level column to test for error
+  test_data <- data.table::copy(example_continuous)[, quantile_level := 0]
+  expect_identical(
+    capture.output(
+      check_data_columns(test_data)
+    ),
+    paste(
+      "[1] \"Found columns `quantile_level` and `sample_id`.",
+      "Only one of these is allowed.\""
+    )
+  )
 })
