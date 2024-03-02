435d31c018643411fad5c1ae87860781bec8c802
nikosbosse <nikosbosse@gmail.com>
2024-03-02 15:43:00 -0600

    Add tests for pit_sample()

--- tests/testthat/test-pit.R
+++ tests/testthat/test-pit.R
@@ -70,3 +70,34 @@ test_that("pit function works for continuous integer and quantile data", {
   expect_s3_class(pit_continuous, c("data.table", "data.frame"), exact = TRUE)
   expect_s3_class(pit_integer, c("data.table", "data.frame"), exact = TRUE)
 })
+
+test_that("pit_sample() throws an error if inputs are wrong", {
+  observed <- rnorm(20, mean = 1:20)
+  predicted <- replicate(100, rnorm(n = 20, mean = 1:20))
+
+  # message if there is only one observation
+  expect_message(
+    expect_equal(
+      pit_sample(observed[1], predicted[1, ]),
+      NA
+    ),
+    "You need more than one observation to assess uniformity of the PIT."
+  )
+
+  # predicted as a data.frame should work
+  expect_no_condition(
+    pit_sample(observed, as.data.frame(predicted))
+  )
+
+  # expect an error if predicted cannot be coerced to a matrix
+  expect_error(
+    pit_sample(observed, function(x) {}),
+    "`predicted` should be a <matrix>"
+  )
+
+  # expect an error if the number of rows in predicted does not match the length of observed
+  expect_error(
+    pit_sample(observed, predicted[1:10, ]),
+    "Mismatch: 'observed' has length `20`, but `predicted` has `10` rows."
+  )
+})
