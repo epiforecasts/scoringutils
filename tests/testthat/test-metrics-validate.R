dc1b6c151aea0f96720f3c3df2323af7d897dedb
nikosbosse <nikosbosse@gmail.com>
2024-03-11 19:12:23 -0400

    fix tests

--- tests/testthat/test-metrics-validate.R
+++ tests/testthat/test-metrics-validate.R
@@ -1,10 +1,16 @@
 test_that("validate_metrics() works as expected", {
+  test_fun <- function(x, y, ...) {
+    if (hasArg("test")) {
+      message("test argument found")
+    }
+    return(y)
+  }
   ## Additional tests for validate_metrics()
   # passing in something that's not a function or a known metric
   expect_warning(
     expect_warning(
-      score(df, metrics = list(
-        "test1" = identity, "test" = test_fun, "hi" = "hi", "2" = 3)
+      score(as_forecast(na.omit(example_binary)), metrics = list(
+        "test1" = test_fun, "test" = test_fun, "hi" = "hi", "2" = 3)
       ),
       "`Metrics` element number 3 is not a valid function"
     ),
