ba07ab513441e7cc8eb6427e72555eae867533b2
nikosbosse <nikosbosse@gmail.com>
2024-03-22 17:45:43 +0100

    Only add present metrics to `metrics` attribute in `as_scores()`

--- R/score.R
+++ R/score.R
@@ -261,7 +261,8 @@ new_scores <- function(scores, metrics, ...) {
 #' @keywords internal
 as_scores <- function(scores, metrics) {
   assert_data_frame(scores)
-  scores <- new_scores(scores, metrics)
+  present_metrics <- metrics[metrics %in% colnames(scores)]
+  scores <- new_scores(scores, present_metrics)
   validate_scores(scores)
   return(scores[])
 }
