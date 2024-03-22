a99794a44f2044565549f0c4605dcc477825817e
nikosbosse <nikosbosse@gmail.com>
2024-03-22 17:41:20 +0100

    Revert "update `as_scores()` to only use the metric names that are actually present in the data"
    This reverts commit 69b44e3a5271cef1185d2c691be9de66d5355741.

--- R/score.R
+++ R/score.R
@@ -261,8 +261,7 @@ new_scores <- function(scores, metrics, ...) {
 #' @keywords internal
 as_scores <- function(scores, metrics) {
   assert_data_frame(scores)
-  present_metrics <- names(metrics)[names(metrics) %in% names(scores)]
-  scores <- new_scores(scores, present_metrics)
+  scores <- new_scores(scores, metrics)
   validate_scores(scores)
   return(scores[])
 }
