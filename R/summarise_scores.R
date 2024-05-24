25ca965209117602b405ab58466baf8aee0a1ce3
Nikos Bosse <37978797+nikosbosse@users.noreply.github.com>
2024-05-24 18:24:22 +0200

    remove across argument from `summarise_scores` (#831)
    * remove across argument from `summarise_scores`
    
    * fix linting issue

--- R/summarise_scores.R
+++ R/summarise_scores.R
@@ -15,18 +15,14 @@
 #'   scores and an additional attribute `metrics` as produced by [score()]).
 #' @param by Character vector with column names to summarise scores by. Default
 #'   is `model`, meaning that there will be one score per model in the output.
-#' @param across Character vector with column names to summarise scores
-#'   across (meaning that the specified columns will be dropped). This is an
-#'   alternative to specifying `by` directly. If `across` is set, `by` will be
-#'   ignored. If `across` is `NULL` (default), then `by` will be used.
 #' @param fun A function used for summarising scores. Default is [mean()].
 #' @param ... Additional parameters that can be passed to the summary function
 #'   provided to `fun`. For more information see the documentation of the
 #'   respective function.
 #' @return
 #' A data.table with summarised scores. Scores are summarised according
-#' to the names of the columns of the original data specified in `by` or
-#' `across` using the `fun` passed to `summarise_scores()`.
+#' to the names of the columns of the original data specified in `by`
+#' using the `fun` passed to `summarise_scores()`.
 #' @examples
 #' \dontshow{
 #'   data.table::setDTthreads(2) # restricts number of cores used on CRAN
@@ -39,11 +35,6 @@
 #'
 #' # get scores by model and target type
 #' summarise_scores(scores, by = c("model", "target_type"))
-#'
-#' # Get scores summarised across horizon, forecast date, and target end date
-#' summarise_scores(
-#'  scores, across = c("horizon", "forecast_date", "target_end_date")
-#' )
 #'
 #' # get standard deviation
 #' summarise_scores(scores, by = "model", fun = sd)
@@ -58,27 +49,15 @@
 
 summarise_scores <- function(scores,
                              by = "model",
-                             across = NULL,
                              fun = mean,
                              ...) {
   # input checking ------------------------------------------------------------
   assert_data_frame(scores)
   scores <- ensure_data.table(scores)
   assert_subset(by, names(scores), empty.ok = TRUE)
-  assert_subset(across, names(scores), empty.ok = TRUE)
   assert_function(fun)
+
   metrics <- get_metrics(scores, error = TRUE)
-
-  forecast_unit <- get_forecast_unit(scores)
-
-  # if across is provided, calculate new `by`
-  if (!is.null(across)) {
-    if (!setequal(by, "model")) {
-      warning("You specified `across` and `by` at the same time.",
-              "`by` will be ignored.")
-    }
-    by <- setdiff(forecast_unit, across)
-  }
 
   # summarise scores -----------------------------------------------------------
   scores <- scores[, lapply(.SD, fun, ...),
