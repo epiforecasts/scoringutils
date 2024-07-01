3407edb26ece9ff7b16a4fffb5e9d96e6c2ef568
nikosbosse <nikosbosse@gmail.com>
2024-07-01 10:58:09 +0200

    fix pkgdown

--- R/forecast.R
+++ R/forecast.R
@@ -60,7 +60,7 @@ NULL
 #' It renames the required columns, where appropriate, and sets the forecast
 #' unit.
 #' @inheritParams as_forecast
-#' @keywords internal
+#' @keywords check-forecasts
 as_forecast_generic <- function(data,
                                 forecast_unit = NULL,
                                 observed = NULL,
