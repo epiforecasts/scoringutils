e27e24e27f06a2bc9af9b9698c89f743c75bee73
Nikos Bosse <37978797+nikosbosse@users.noreply.github.com>
2024-10-09 11:41:11 +0100

    Update R/z-packageStartupMessage.R
    Co-authored-by: James Azam <james.m.azam@gmail.com>

--- R/z-packageStartupMessage.R
+++ R/z-packageStartupMessage.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "We'd like to hear your feedback and thoughts! Let us know by ",
-    "creating an issue on https://github.com/epiforecasts/scoringutils."
+    "creating an issue on {.url https://github.com/epiforecasts/scoringutils/issues}."
   )
 }
