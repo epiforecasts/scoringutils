798722a74fc63dc79187d7e75dad138f82fd6376
nikosbosse <nikosbosse@gmail.com>
2024-10-09 10:44:09 +0200

    update startup message and rename file

---
+++ R/z-packageStartupMessage.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "We'd like to hear your feedback and thoughts! Let us know by ",
+    "creating an issue on https://github.com/epiforecasts/scoringutils."
+  )
+}
