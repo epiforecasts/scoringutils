f419aa1c66800902c6fb6431808f1dd3d2cf20b8
nikosbosse <nikosbosse@gmail.com>
2025-03-31 23:03:57 +0200

    allow for numeric imprecision

--- R/metrics-nominal.R
+++ R/metrics-nominal.R
@@ -72,9 +72,12 @@ assert_input_categorical <- function(
     assert_matrix(predicted, nrows = n)
     summed_predictions <- round(rowSums(predicted, na.rm = TRUE), 10) # avoid numeric errors
   }
-  if (!all(summed_predictions == 1)) {
+  # Allow for numeric errors
+  tolerance <- 1e-8
+  invalid_rows <- abs(summed_predictions - 1) > tolerance
+  if (any(invalid_rows)) {
     #nolint start: keyword_quote_linter object_usage_linter
-    row_indices <- as.character(which(summed_predictions != 1))
+    row_indices <- which(invalid_rows)
     cli_abort(
       c(
         `!` = "Probabilities belonging to a single forecast must sum to one",
