81d508a436d2a677076700207945daa17ed5dadb
nikosbosse <nikosbosse@gmail.com>
2025-08-25 17:00:20 +0200

    comment out hexsticker code, update logo

--- inst/make_hexsticker.R
+++ inst/make_hexsticker.R
@@ -1,77 +1,80 @@
-library(hexSticker)
-library(ggplot2)
-library(dplyr)
-library(magick)
-library(scoringutils)
+# library(hexSticker)
+# library(ggplot2)
+# library(dplyr)
+# library(magick)
+# library(scoringutils)
 
-# Make standard plot
-scores <- example_quantile %>%
-  as_forecast_quantile() %>%
-  score() %>%
-  summarise_scores(by = c("model", "target_type")) |>
-  filter(target_type == "Cases")
+# Note: the final sticker was based on this and lightly edited using ChatGPT.
 
-hex_plot <- plot_wis(scores, x = "model", relative_contributions = FALSE) +
-  facet_wrap(~target_type, scales = "free_x") +
-  labs(x = "", y = "") +
-  theme(
-    legend.position = "none",
-    strip.text = element_blank(),
-    axis.text.x = element_blank(),
-    axis.ticks.x = element_blank(),
-    axis.text.y = element_blank()
-  )
 
-# Make and save hexsticker
-sticker(
-  hex_plot,
-  s_x = 0.93,
-  s_y = 0.82,
-  s_width = 1.6,
-  s_height = 1.1,
-  package = "scoringutils",
-  p_color = "#646770",
-  p_size = 76,
-  p_x = 1,
-  p_y = 1.5,
-  h_fill = "#ffffff",
-  h_color = "#646770",
-  filename = file.path("man", "figures", "logo.png"),
-  dpi = 1200,
-  white_around_sticker = TRUE
-)
+# # Make standard plot
+# scores <- example_quantile %>%
+#   as_forecast_quantile() %>%
+#   score() %>%
+#   summarise_scores(by = c("model", "target_type")) |>
+#   filter(target_type == "Cases")
 
-# Make outside of hex sticker transparent
-p <- image_read(file.path("man", "figures", "logo.png"))
-fuzz <- 50
+# hex_plot <- plot_wis(scores, x = "model", relative_contributions = FALSE) +
+#   facet_wrap(~target_type, scales = "free_x") +
+#   labs(x = "", y = "") +
+#   theme(
+#     legend.position = "none",
+#     strip.text = element_blank(),
+#     axis.text.x = element_blank(),
+#     axis.ticks.x = element_blank(),
+#     axis.text.y = element_blank()
+#   )
 
-pp <- p |>
-  image_fill(
-    color = "transparent",
-    refcolor = "white",
-    fuzz = fuzz,
-    point = "+1+1"
-  ) |>
-  image_fill(
-    color = "transparent",
-    refcolor = "white",
-    fuzz = fuzz,
-    point = paste0("+", image_info(p)$width - 1, "+1")
-  ) |>
-  image_fill(
-    color = "transparent",
-    refcolor = "white",
-    fuzz = fuzz,
-    point = paste0("+1", "+", image_info(p)$height - 1)
-  ) |>
-  image_fill(
-    color = "transparent",
-    refcolor = "white",
-    fuzz = fuzz,
-    point = paste0("+", image_info(p)$width - 1, "+", image_info(p)$height - 1)
-  )
+# # Make and save hexsticker
+# sticker(
+#   hex_plot,
+#   s_x = 0.93,
+#   s_y = 0.82,
+#   s_width = 1.6,
+#   s_height = 1.1,
+#   package = "scoringutils",
+#   p_color = "#646770",
+#   p_size = 76,
+#   p_x = 1,
+#   p_y = 1.5,
+#   h_fill = "#ffffff",
+#   h_color = "#646770",
+#   filename = file.path("man", "figures", "logo.png"),
+#   dpi = 1200,
+#   white_around_sticker = TRUE
+# )
 
-image_write(image = pp, path = file.path("man", "figures", "logo.png"))
+# # Make outside of hex sticker transparent
+# p <- image_read(file.path("man", "figures", "logo.png"))
+# fuzz <- 50
 
-usethis::use_logo(file.path("man", "figures", "logo.png"))
-pkgdown::build_favicons(pkg = ".", overwrite = TRUE)
+# pp <- p |>
+#   image_fill(
+#     color = "transparent",
+#     refcolor = "white",
+#     fuzz = fuzz,
+#     point = "+1+1"
+#   ) |>
+#   image_fill(
+#     color = "transparent",
+#     refcolor = "white",
+#     fuzz = fuzz,
+#     point = paste0("+", image_info(p)$width - 1, "+1")
+#   ) |>
+#   image_fill(
+#     color = "transparent",
+#     refcolor = "white",
+#     fuzz = fuzz,
+#     point = paste0("+1", "+", image_info(p)$height - 1)
+#   ) |>
+#   image_fill(
+#     color = "transparent",
+#     refcolor = "white",
+#     fuzz = fuzz,
+#     point = paste0("+", image_info(p)$width - 1, "+", image_info(p)$height - 1)
+#   )
+
+# image_write(image = pp, path = file.path("man", "figures", "logo.png"))
+
+# usethis::use_logo(file.path("man", "figures", "logo.png"))
+# pkgdown::build_favicons(pkg = ".", overwrite = TRUE)
